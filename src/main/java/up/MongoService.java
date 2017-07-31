98e17ed8e2d4cdd26f96a31c5acad21a9ac467c3
Valery Sorokin <netsquire@gmail.com>
2017-07-31 15:55:22 +0200

    add Mongo

--- src/main/java/up/MongoService.java
+++ src/main/java/up/MongoService.java
@@ -3,19 +3,21 @@ package up;
 import java.net.UnknownHostException;
 
 import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 
 public class MongoService {
 
 	String dbName = "weblocator";
-	MongoClient mongoClient;
+	String collectionName = "grid";
+	MongoClient mongoClient = null;
+	DBCollection collection = null;
 	
 	public MongoService(){
 		
 		try {
 			mongoClient = new MongoClient("127.0.0.1", 27017);
+			collection = mongoClient.getDB(dbName).getCollection("collectionName");
 		} catch (UnknownHostException e) {
 			System.out.println(e.getMessage());
 			//e.printStackTrace();
@@ -26,9 +28,7 @@ public class MongoService {
 	public void putIp(String id, String ip){
 		BasicDBObject ipDoc = new BasicDBObject();
 		ipDoc.put("id", id);
-		ipDoc.put("ip", ip);
-		DB db = mongoClient.getDB(dbName);
-		DBCollection collection = db.getCollection("local");
+		ipDoc.put("ip", ip);		
 		collection.insert(ipDoc);
 	}
 	
