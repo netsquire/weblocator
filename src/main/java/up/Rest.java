4b8261042db7afa3920e4968bf59c150d856daf1
Valery Sorokin <netsquire@gmail.com>
2017-07-31 13:51:59 +0200

    modify Rest

--- src/main/java/up/Rest.java
+++ src/main/java/up/Rest.java
@@ -7,17 +7,18 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 
-@Path("hello")
+@Path("/")
 public class Rest {
 	
     @GET
+    @Path("/empty")
     @Produces(MediaType.TEXT_PLAIN)
     public String emptyId() {
         return "Empty request";
     }
 
     @GET
-    @Path("{id}")
+    @Path("/info/{id}")
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt(@PathParam("id") String id) {
         return "Got it - " + id;
