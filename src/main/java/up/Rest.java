0328e9cbe9ee4806b98cad53f1a9775e65166327
Valery Sorokin <netsquire@gmail.com>
2017-07-31 15:59:25 +0200

    add Mongo

--- src/main/java/up/Rest.java
+++ src/main/java/up/Rest.java
@@ -6,38 +6,40 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-
 @Path("/")
 public class Rest {
-	
-	MongoService mongoService = new MongoService();
-	
-    @GET
-    @Path("/")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String index() {
-        return "request: /info/{id}";
-    }
-    
-    @GET
-    @Path("/empty")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String emptyId() {
-        return "Empty request";
-    }
 
-    @GET
-    @Path("/info/{id}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt(@PathParam("id") String id) {
-        return "Got it - " + id;
-    }
-    
-    @GET
-    @Path("/{id}/ip/{ip}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String ipAnnounce(@PathParam("id") String id, @PathParam("ip") String ip) {
-    	mongoService.putIp(id, ip);
-        return "Got it - " + id;
-    }
+	MongoService mongoService;
+	public Rest() {
+		mongoService = new MongoService();
+	}
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String index() {
+		return "request: /info/{id}";
+	}
+
+	@GET
+	@Path("/empty")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String emptyId() {
+		return "Empty request";
+	}
+
+	@GET
+	@Path("/info/{id}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getIt(@PathParam("id") String id) {
+		return "Got it - " + id;
+	}
+
+	@GET
+	@Path("/{id}/ip/{ip}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String ipAnnounce(@PathParam("id") String id, @PathParam("ip") String ip) {
+		mongoService.putIp(id, ip);
+		return "Got it - " + id;
+	}
 }
