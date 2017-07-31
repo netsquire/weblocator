f10999434c56ceff29c0dc625cda7b42b7946994
Valery Sorokin <netsquire@gmail.com>
2017-07-31 16:29:41 +0200

    add Mongo

--- src/main/java/up/MongoService.java
+++ src/main/java/up/MongoService.java
@@ -3,6 +3,7 @@ package up;
 import java.net.UnknownHostException;
 
 import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 
@@ -12,12 +13,22 @@ public class MongoService {
 	String collectionName = "grid";
 	MongoClient mongoClient = null;
 	DBCollection collection = null;
+	DB db = null;
 	
-	public MongoService(){
-		
+	public MongoService() throws Exception{
+		dbName = System.getProperty("OPENSHIFT_APP_NAME");
+		String username = System.getProperty("OPENSHIFT_MONGODB_DB_USERNAME");
+		String password = System.getProperty("OPENSHIFT_MONGODB_DB_PASSWORD");
 		try {
 			mongoClient = new MongoClient("127.0.0.1", 27017);
-			collection = mongoClient.getDB(dbName).getCollection(collectionName);
+			db = mongoClient.getDB(dbName);
+			collection = db.getCollection(collectionName);
+			if(db.authenticate(username, password.toCharArray())){
+				System.out.println("Auth is OK.");
+				}
+			else {
+				throw new Exception("AUTH IS NOT OK.");
+			}
 		} catch (UnknownHostException e) {
 			//System.out.println(e.getMessage());
 			e.printStackTrace();
