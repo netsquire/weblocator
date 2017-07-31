a0500f7d41d69de8d84f546aec8764099b3fa1b4
Valery Sorokin <netsquire@gmail.com>
2017-07-31 17:18:00 +0200

    add Mongo

--- src/main/java/up/MongoService.java
+++ src/main/java/up/MongoService.java
@@ -1,14 +1,10 @@
 package up;
 
 import java.net.UnknownHostException;
-
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
-import com.mongodb.MongoClientOptions;
-import com.mongodb.ServerAddress;
-import com.mongodb.WriteConcern;
 
 public class MongoService {
 
@@ -19,31 +15,21 @@ public class MongoService {
 	DB db = null;
 
 	public MongoService() {
-		// dbName = System.getProperty("OPENSHIFT_APP_NAME");
-		// String username =
-		// System.getProperty("OPENSHIFT_MONGODB_DB_USERNAME");
-		// String password =
-		// System.getProperty("OPENSHIFT_MONGODB_DB_PASSWORD");
+		dbName = System.getProperty("OPENSHIFT_APP_NAME");
+		String username = System.getProperty("OPENSHIFT_MONGODB_DB_USERNAME");
+		String password = System.getProperty("OPENSHIFT_MONGODB_DB_PASSWORD");
 		String host = System.getenv("OPENSHIFT_MONGODB_DB_HOST");
 		String port = System.getenv("OPENSHIFT_MONGODB_DB_PORT");
 		String dbName = System.getenv("OPENSHIFT_APP_NAME");
-		//MongoClientOptions mongoClientOptions = MongoClientOptions.builder().build();
-        //MongoClient mongoClient = new MongoClient(new ServerAddress(host, 27017), mongoClientOptions);
-        mongoClient.setWriteConcern(WriteConcern.SAFE);
+
 		try {
-			mongoClient = new MongoClient(host, 27017 );
+			mongoClient = new MongoClient(host, 27017);
 			db = mongoClient.getDB(dbName);
 			collection = db.getCollection(collectionName);
-			/*
-			 * if(db.authenticate(username, password.toCharArray())){
-			 * System.out.println("Auth is OK."); } else {
-			 * System.out.println("AUTH IS NOT OK."); }
-			 */
 		} catch (UnknownHostException e) {
 			// System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
-
 	}
 
 	public void putIp(String id, String ip) {
