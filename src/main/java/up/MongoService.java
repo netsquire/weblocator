648c2e0f26eee8e2dab8ff60113eb15cdd8afe17
Valery Sorokin <netsquire@gmail.com>
2017-07-31 17:57:56 +0200

    add Mongo

--- src/main/java/up/MongoService.java
+++ src/main/java/up/MongoService.java
@@ -23,14 +23,13 @@ public class MongoService {
 		String dbName = System.getenv("OPENSHIFT_APP_NAME");
 
 		try {
-			mongoClient = new MongoClient(); //"mongodb://admin:xcY_Iv7IZzRw@" + host + "27017/" + dbName);
+			mongoClient = new MongoClient("mongodb://admin:xcY_Iv7IZzRw@" + host + ":27017/");
 			db = mongoClient.getDB(dbName);
 //			if (!db.authenticate(username, password.toCharArray())){
 //				System.out.println("NO AUTH");
 //			}
 			collection = db.getCollection(collectionName);
 		} catch (UnknownHostException e) {
-			// System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
 	}
