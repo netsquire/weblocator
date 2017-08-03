97a8c36c36b1f7b7efcac87f01e81244e5f9ca1a
Valery Sorokin <netsquire@gmail.com>
2017-08-03 21:29:11 +0200

    add Mongo

--- src/main/java/up/MongoService.java
+++ src/main/java/up/MongoService.java
@@ -1,20 +1,29 @@
 package up;
 
 import java.net.UnknownHostException;
+import java.util.Arrays;
+
+import org.bson.Document;
+
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class MongoService {
 
 	String dbName = "weblocator";
 	static String collectionName = "grid";
 	MongoClient mongoClient = null;
-	static DBCollection collection = null;
-	static DB db = null;
+	MongoCollection<Document> collection;
+	//static DB db = null;
 
 	public MongoService() {
 		dbName = System.getProperty("OPENSHIFT_APP_NAME");
@@ -23,26 +32,25 @@ public class MongoService {
 		String host = System.getenv("OPENSHIFT_MONGODB_DB_HOST");
 		String port = System.getenv("OPENSHIFT_MONGODB_DB_PORT");
 		String dbName = System.getenv("OPENSHIFT_APP_NAME");
-		try {
-			mongoClient = new MongoClient(host, 27017);
-			db = mongoClient.getDB(dbName);
-			collection = db.getCollection(collectionName);
-		} catch (UnknownHostException e) { e.printStackTrace();}
+
+		MongoCredential credential = MongoCredential.createCredential(username, dbName, password.toCharArray());
+		mongoClient = new MongoClient(new ServerAddress(host, 27017), Arrays.asList(credential));
+		MongoDatabase db = mongoClient.getDatabase(dbName);
+		collection = db.getCollection(collectionName);
 	}
 
 	public void putIp(String id, String ip) {
-		BasicDBObject ipDoc = new BasicDBObject();
+		Document ipDoc = new Document();
 		ipDoc.put("id", id);
 		ipDoc.put("ip", ip);
-		collection.insert(ipDoc);
+		collection.insertOne(ipDoc);
 	}
 
 	public static void main(String[] args) throws UnknownHostException {
 		String mongoServer = "192.168.0.101";
 		MongoClient ms = new MongoClient(mongoServer, 27017);
-		db = ms.getDB("weblocator");
-		collection = db.getCollection(collectionName);
-		BasicDBObject query = new BasicDBObject();
+		
+	/*	BasicDBObject query = new BasicDBObject();
 		query.put("", "");
 		DBCursor cursor = collection.find(null);
 		while (cursor.hasNext()) {
@@ -54,6 +62,6 @@ public class MongoService {
 		DBObject dbo = collection.findOne();
 		System.out.println(dbo);
 		
-		
+		*/
 	}
 }
